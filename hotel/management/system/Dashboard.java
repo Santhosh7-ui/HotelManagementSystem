583eb706c78a7339accf848cff7992931f704670
santhoshsanthosh <santhosh.puttaraju1@gmail.com>
2025-05-04 10:07:25 +0530

    dashbord

--- hotel/management/system/Dashboard.java
+++ hotel/management/system/Dashboard.java
@@ -11,7 +11,7 @@ public class Dashboard extends JFrame {
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/third.jpg"));
         Image i2 = i1.getImage().getScaledInstance(1550,1000,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
-        JLabel image = new JLabel();
+        JLabel image = new JLabel(i3);
         image.setBounds(0,0,1550,1000);
         add(image);
 
