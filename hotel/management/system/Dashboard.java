fbde47e874f157a58d370ed5c747958f815d8f87
santhoshsanthosh <santhosh.puttaraju1@gmail.com>
2025-05-04 19:59:08 +0530

    Completed the Dashboard.java

--- hotel/management/system/Dashboard.java
+++ hotel/management/system/Dashboard.java
@@ -8,15 +8,43 @@ public class Dashboard extends JFrame {
     Dashboard(){
         setBounds(0,0,1550,1000);
         setLayout(null);
+
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/third.jpg"));
         Image i2 = i1.getImage().getScaledInstance(1550,1000,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
-        JLabel image = new JLabel();
+        JLabel image = new JLabel(i3);
         image.setBounds(0,0,1550,1000);
         add(image);
 
         JLabel text = new JLabel("TAJ Group Welcome You");
-        text.setBounds(400,80,1000,50);
+        text.setBounds(500,200,1000,50);
+        text.setForeground(Color.black);
+        text.setFont(new Font("serif",Font.PLAIN,40));
+        System.out.println("Hi..");
+        image.add(text);
+
+        JMenuBar menu = new JMenuBar();
+        menu.setBounds(0,0,1550,30);
+        image.add(menu);
+
+        JMenu hotel = new JMenu("Hotel Management System");
+        hotel.setForeground(Color.RED);
+        menu.add(hotel);
+
+        JMenuItem reception = new JMenuItem("Reception");
+        hotel.add(reception);
+
+        JMenu admin = new JMenu("Admin");
+        menu.add(admin);
+
+        JMenuItem addDriver = new JMenuItem("ADD Drivers");
+        admin.add(addDriver);
+
+        JMenuItem addRooms = new JMenuItem("ADD Rooms");
+        admin.add(addRooms);
+
+        JMenuItem addEmployee = new JMenuItem("ADD Employee");
+        admin.add(addEmployee);
 
         setVisible(true);
     }
